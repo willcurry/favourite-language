75bffed6ac5184f529504f8eb1b6069ed54550eb
Will Curry <watercrafthd@gmail.com>
2018-03-24 17:57:33 +0000

    Display favourite language above top 3

--- spec/controllers/languages_controller_spec.rb
+++ spec/controllers/languages_controller_spec.rb
@@ -16,19 +16,24 @@ RSpec.describe LanguagesController do
     LanguagesController.set :languages, languages
   end
 
-  it "post /search route returns ok response" do
+  it "post /top returns ok response" do
     post "/top"
     expect(last_response).to be_ok
   end
 
-  it "post /search displays top 3 languages" do
+  it "post /top displays top 3 languages" do
     post "/top"
     expected = "<p>Language: ruby Bytes: 6000</p>\n  <p>Language: python Bytes: 500</p>\n  <p>Language: html Bytes: 100</p>"
     expect(last_response.body).to include(expected)
   end
 
-  it "post /search displays username in question" do
+  it "post /top displays username in question" do
     post "/top", {:username => "willcurry"}
     expect(last_response.body).to include("willcurry")
+  end
+
+  it "post /top displays their favourite language" do
+    post "/top", {:username => "willcurry"}
+    expect(last_response.body).to include("ruby is their favourite")
   end
 end
